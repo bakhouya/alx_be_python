77418937bd74516431dfbe416ba881b6c69afc7d
bamoos <kamfour1997@gmail.com>
2025-09-08 13:15:34 +0200

    Added basic python instructions: addition, subtraction, multiolication

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,32 @@
+
+# ==========================================================================================
+# Here we define the variable number1 and assign it the value 10, which is an integer
+number1 = 10
+# Here we define the variable number2 and assign it the value 5, which is an integer
+number2 = 5
+# ==========================================================================================
+
+
+# ==========================================================================================
+# Here we add number1 and number2, then store the result in the variable addition
+addition = number1 + number2
+
+# Here we subtract number2 from number1, then store the result in the variable subtraction
+subtraction = number1 - number2
+
+# Here we multiply number1 by number2, then store the result in the variable multiplication
+multiplication = number1 * number2
+# ========================================================================================== 
+
+
+# ==========================================================================================
+# Here we are printing a sentence that shows the result of adding number1 and number2
+print(f"Addition of {number1} and {number2} is {addition}")
+
+# Here we are printing a sentence that shows the result of subtracting number2 from number1
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+
+# Here we are printing a sentence that shows the result of multiplying number1 by number2
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
+# ==========================================================================================
+
